0467f552e72594e6b47a61b4b784512a0d64dec2
Pranav Nigam <pranav.nigam11@gmail.com>
2019-02-20 21:37:04 -0800

    Initial Commit

---
+++ add_face.py
@@ -0,0 +1,70 @@
+# Import Libraries
+import cv2
+import os
+
+directory = input("Enter your name: ").title().replace(" ", "-")
+directory = "../Images/" + directory
+
+try:
+	os.mkdir(directory)
+except FileExistsError:
+	print("The folder %s already exists! Continuing program." % directory)
+else:
+	print("Succesfully created folder %s!" % directory)
+
+# Specify the Classifier for the Cascade
+face_cascade = cv2.CascadeClassifier('../Cascades/data/haarcascade_frontalface_default.xml')
+
+# Start Capturing Video from Default Webcam
+cap = cv2.VideoCapture(0)
+
+count = 0
+num_pics = 100
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+	# Convert to grayscale (black and white)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Finding Faces
+	faces = face_cascade.detectMultiScale(gray)
+	for (x, y, w, h) in faces:
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+h]
+
+		count += 1
+		file = directory + "/" + str(count) + ".png"
+		cv2.imwrite(file, roi_color)
+
+		# Display count
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		font_size = 0.75
+		text = str(count) + "/" + str(num_pics)
+		color = (255, 255, 255)
+		stroke = 2
+		cv2.putText(frame, text, (x, y), font, font_size, color, stroke, cv2.LINE_AA)
+
+		# Draw Rectangle around face
+		color = (255, 255, 0) # BGR -> cyan
+		stroke = 3
+		x_end = x + w
+		y_end = y + h
+		cv2.rectangle(frame, (x, y), (x_end, y_end), color, stroke)
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+
+	# Stop if user presses 'q' or 50 pictures have been taken
+	if (cv2.waitKey(20) & 0xFF == ord('q')) or count == num_pics:
+		break
+
+# Stop getting webcam input and close all windows at the end of the program
+cap.release()
+cv2.destroyAllWindows()
+
+if count == num_pics:
+	print("Face Data Collection Complete")
+else:
+	print("Canceled")
---
+++ faces.py
@@ -0,0 +1,77 @@
+# Import Libraries
+import cv2
+import numpy as np
+from PIL import Image
+import pickle
+
+# Specify the Classifier for the Cascade
+face_cascade = cv2.CascadeClassifier('../Cascades/data/haarcascade_frontalface_default.xml')
+
+# Initialize the recognizer
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+labels = {}
+with open("labels.pickle", 'rb') as f:
+	# Load File
+	labels = pickle.load(f)
+	
+	# Invert dictionary (switch keys with values)
+	labels = {value:key for key, value in labels.items()}
+
+# Start Capturing Video from Default Webcam
+cap = cv2.VideoCapture(0)
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+	# Convert to grayscale (black and white)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	# Finding Faces
+	faces = face_cascade.detectMultiScale(gray)
+	for (x, y, w, h) in faces:
+		# print(x, y, w, h)
+
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+h]
+
+		# Convert to picture, Resize, Convert back to array
+		img = Image.fromarray(roi_gray, 'L').resize((100, 100), Image.ANTIALIAS)
+		roi_gray = np.array(img, "uint8")
+
+		# Recognize
+		id_, conf = recognizer.predict(roi_gray)
+		if conf <= 95:
+			print(labels[id_], "\t", conf)
+
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			font_size = 0.75
+			text = labels[id_]
+			color = (0, 0, 255)
+			stroke = 2
+			cv2.putText(frame, text, (x, y), font, font_size, color, stroke, cv2.LINE_AA)
+			cv2.putText(frame, str(round(conf, 3)), (x, int(y+h+20)), font, font_size, color, stroke, cv2.LINE_AA)
+
+		# Save picture of cropped face in a file called "my-image.png"
+		img_item = "my-image.png"
+		cv2.imwrite(img_item, roi_color)
+
+		# Draw Rectangle around face
+		color = (255, 255, 0) # BGR -> cyan
+		stroke = 3
+		x_end = x + w
+		y_end = y + h
+		cv2.rectangle(frame, (x, y), (x_end, y_end), color, stroke)
+
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+
+	# Stop if user presses 'q'
+	if cv2.waitKey(20) & 0xFF == ord('q'):
+		break
+
+# Stop getting webcam input and close all windows at the end of the program
+cap.release()
+cv2.destroyAllWindows()
