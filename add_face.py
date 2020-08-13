d01f29d3271b8456d2c39ff59e4741eba301e7fd
Pranav Nigam <pranav.nigam11@gmail.com>
2020-08-12 21:16:04 -0700

    v2.0.0 - Screen capture intergration

--- add_face.py
+++ add_face.py
@@ -4,37 +4,23 @@
 import cv2
 import os
 import time
-
-name = input("Enter your name: ").title()
-directory = name.replace(" ", "-")
-directory = "../Images/" + directory
-
-try:
-	os.mkdir(directory)
-except FileExistsError:
-	print("The folder %s already exists! Overwriting." % directory)
-else:
-	print("Succesfully created folder %s!" % directory)
-
-# Specify the Classifier for the Cascade
-face_cascade = cv2.CascadeClassifier('./Cascades/data/haarcascade_frontalface_default.xml')
-start_time = time.time()
-
-# Start Capturing Video from Default Webcam
-cap = cv2.VideoCapture(0)
+import numpy as np
+import pyscreenshot as ImageGrab # LINUX
+# from PIL import ImageGrab # WINDOWS/MAC
 
 count = 0
-num_pics = 100
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_size = 0.75
-white = (255, 255, 255)
-cyan = (255, 255, 0)
-red = (0, 0, 255)
+def input_method():
+	face_input = input("Record from screen or webcam: ").lower()
+	if "screen" in face_input or "display" in face_input:
+		return "screen"
+	elif "cam" in face_input:
+		return "webcam"
+	else:
+		return input_method()
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
+def process_frame(frame):
+	global count
 
 	# Convert to grayscale (black and white)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -75,10 +61,90 @@ while(True):
 		x_end = x + w
 		y_end = y + h
 		cv2.rectangle(frame, (x, y), (x_end, y_end), cyan, 3)
-	
+	return frame
+
+name = input("Enter your name: ").title()
+directory = name.replace(" ", "-")
+directory = "../Images/" + directory
+
+try:
+	os.mkdir(directory)
+except FileExistsError:
+	print("The folder %s already exists! Overwriting." % directory)
+else:
+	print("Succesfully created folder %s!" % directory)
+
+input = input_method()
+start_time = time.time()
+
+# Specify the Classifier for the Cascade
+face_cascade = cv2.CascadeClassifier('./Cascades/data/haarcascade_frontalface_default.xml')
+
+# Start Capturing Video from Default Webcam
+cap = cv2.VideoCapture(0)
+
+num_pics = 100
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 0.75
+white = (255, 255, 255)
+cyan = (255, 255, 0)
+red = (0, 0, 255)
+
+while(True):
+	# Capture frame-by-frame
+	if input == "screen":
+		screen_pil = ImageGrab.grab(bbox=(0, 0, 1600, 900))
+		screen_np = np.array(screen_pil)
+		frame = cv2.cvtColor(screen_np, cv2.COLOR_BGR2RGB)#.resize((720, 480), Image.ANTIALIAS)
+	else:
+		ret, frame = cap.read()
+
+	# # Convert to grayscale (black and white)
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#
+	# # Finding Faces
+	# faces = face_cascade.detectMultiScale(gray)
+	#
+	# num_faces = len(faces)
+	#
+	# # Give Message if No Faces are Detected
+	# if num_faces == 0:
+	# 	text1 = "No Face Detected!"
+	# 	text2 = "Please make sure " + name + " is in frame."
+	# 	cv2.putText(frame, text1, (0, 20), font, font_size, red, 2, cv2.LINE_AA)
+	# 	cv2.putText(frame, text2, (0, 40), font, font_size, red, 2, cv2.LINE_AA)
+	#
+	# for (x, y, w, h) in faces:
+	# 	roi_gray = gray[y:y+h, x:x+w]
+	# 	roi_color = frame[y:y+h, x:x+h]
+	#
+	# 	# Give Message and Pause Data Collection if Multiple Faces are Detected
+	# 	if num_faces == 1:
+	# 		count += 1
+	# 		file = directory + "/" + str(count) + ".png"
+	# 		cv2.imwrite(file, roi_color)
+	# 	else:
+	# 		text1 = str(num_faces) + " Faces Detected! Data Collection Paused."
+	# 		text2 = "Please make sure only " + name + " is in frame."
+	# 		cv2.putText(frame, text1, (0, 20), font, font_size, red, 2, cv2.LINE_AA)
+	# 		cv2.putText(frame, text2, (0, 40), font, font_size, red, 2, cv2.LINE_AA)
+	#
+	#
+	# 	# Display count
+	# 	text = str(count) + "/" + str(num_pics)
+	# 	cv2.putText(frame, text, (x, y), font, font_size, white, 2, cv2.LINE_AA)
+	#
+	# 	# Draw Rectangle around face
+	# 	x_end = x + w
+	# 	y_end = y + h
+	# 	cv2.rectangle(frame, (x, y), (x_end, y_end), cyan, 3)
+
+	processed_frame = process_frame(frame)
+	#processed_screen = cv2.resize(processed_screen_full, dsize=(800, 450), interpolation=cv2.INTER_LINEAR)
 
 	# Display the resulting frame
-	cv2.imshow('frame', frame)
+	cv2.imshow(input.title(), processed_frame)
 
 	# Stop if user presses 'q' or specified numebr of pictures have been taken
 	if (cv2.waitKey(20) & 0xFF == ord('q')) or count >= num_pics:
@@ -91,7 +157,7 @@ cv2.destroyAllWindows()
 if count >= num_pics:
 	print("Face Data Collection Complete")
 else:
-	print("Canceled")
+	print("Cancelled")
 
 end_time = time.time()
 total_time = end_time - start_time
--- faces.py
+++ faces.py
@@ -3,31 +3,13 @@
 # Import Libraries
 import cv2
 import numpy as np
-from PIL import Image
 import pickle
+from PIL import Image
+import pyscreenshot as ImageGrab # LINUX
+# from PIL import ImageGrab # WINDOWS/MAC
 
-# Specify the Classifier for the Cascade
-face_cascade = cv2.CascadeClassifier('./Cascades/data/haarcascade_frontalface_default.xml')
-
-# Initialize the recognizer
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("../trainer.yml")
-
-labels = {}
-with open("../labels.pickle", 'rb') as f:
-	# Load File
-	labels = pickle.load(f)
-	
-	# Invert dictionary (switch keys with values)
-	labels = {value:key for key, value in labels.items()}
-
-# Start Capturing Video from Default Webcam
-cap = cv2.VideoCapture(0)
-
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
-
+# Function to process frame
+def process_frame(frame):
 	# Convert to grayscale (black and white)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -66,14 +48,85 @@ while(True):
 		x_end = x + w
 		y_end = y + h
 		cv2.rectangle(frame, (x, y), (x_end, y_end), color, stroke)
+	return frame
+
+
+# Specify the Classifier for the Cascade
+face_cascade = cv2.CascadeClassifier('./Cascades/data/haarcascade_frontalface_default.xml')
+
+# Initialize the recognizer
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("../trainer.yml")
+
+labels = {}
+with open("../labels.pickle", 'rb') as f:
+	# Load File
+	labels = pickle.load(f)
+
+	# Invert dictionary (switch keys with values)
+	labels = {value:key for key, value in labels.items()}
+
+# Start Capturing Video from Default Webcam
+webcam_capture = cv2.VideoCapture(0)
+
+while True:
+	## Capture frame-by-frame
+	# Capture Webcam
+	ret, webcam_frame = webcam_capture.read()
+	# Capture Screen
+	screen_pil = ImageGrab.grab(bbox=(0, 0, 1600, 900))
+	screen_np = np.array(screen_pil)
+	screen_frame = cv2.cvtColor(screen_np, cv2.COLOR_BGR2RGB)#.resize((720, 480), Image.ANTIALIAS)
+
+	# # Convert to grayscale (black and white)
+	# webcam_gray = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
+	#
+	# # Finding Faces
+	# faces = face_cascade.detectMultiScale(webcam_gray)
+	# for (x, y, w, h) in faces:
+	# 	# print(x, y, w, h)
+	#
+	# 	roi_gray = webcam_gray[y:y+h, x:x+w]
+	# 	roi_color = webcam_frame[y:y+h, x:x+h]
+	#
+	# 	# Convert to picture, Resize, Convert back to array
+	# 	img = Image.fromarray(roi_gray, 'L').resize((100, 100), Image.ANTIALIAS)
+	# 	roi_gray = np.array(img, "uint8")
+	#
+	# 	# Recognize
+	# 	id_, loss = recognizer.predict(roi_gray)
+	# 	if loss <= 95:
+	# 		print(labels[id_], "\t", loss)
+	#
+	# 		font = cv2.FONT_HERSHEY_SIMPLEX
+	# 		font_size = 0.75
+	# 		text = labels[id_]
+	# 		color = (0, 0, 255)
+	# 		stroke = 2
+	# 		cv2.putText(webcam_frame, text, (x, y), font, font_size, color, stroke, cv2.LINE_AA)
+	# 		cv2.putText(webcam_frame, str(round(loss, 3)), (x, int(y+h+20)), font, font_size, color, stroke, cv2.LINE_AA)
+
+		# # Save picture of cropped face in a file called "my-image.png"
+		# img_item = "my-image.png"
+		# cv2.imwrite(img_item, roi_color)
+		# # Draw Rectangle around face
+		# color = (255, 255, 0) # BGR -> cyan
+		# stroke = 3
+		# x_end = x + w
+		# y_end = y + h
+		# cv2.rectangle(webcam_frame, (x, y), (x_end, y_end), color, stroke)
+	processed_webcam = process_frame(webcam_frame)
+	processed_screen_full = process_frame(screen_frame)
+	processed_screen = cv2.resize(processed_screen_full, dsize=(800, 450), interpolation=cv2.INTER_LINEAR)
 
 	# Display the resulting frame
-	cv2.imshow('frame', frame)
+	cv2.imshow('Webcam', processed_webcam)
+	cv2.imshow('Screen', processed_screen)
 
 	# Stop if user presses 'q'
 	if cv2.waitKey(20) & 0xFF == ord('q'):
 		break
 
 # Stop getting webcam input and close all windows at the end of the program
-cap.release()
+webcam_capture.release()
 cv2.destroyAllWindows()
